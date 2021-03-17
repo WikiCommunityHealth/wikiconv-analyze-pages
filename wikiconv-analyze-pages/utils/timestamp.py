b2ffa8cf5f4c282b7ab25942892efc52e3b7b2ba
nicolatoscan <nicolatoscan99@outlook.it>
2021-03-17 03:10:52 +0100

    removed log

--- wikiconv-analyze-pages/utils/timestamp.py
+++ wikiconv-analyze-pages/utils/timestamp.py
@@ -7,7 +7,6 @@ def setOutputPath(outputPath: Path):
     timesOutputPath = outputPath
 
 def printTimestamp(description: str) -> None:
-    print(str(timesOutputPath / 'times.txt'))
     with open(str(timesOutputPath / 'times.txt'), "a") as f:
         line = f'{datetime.now().strftime("%H:%M:%S")}: {description}\n'
         f.write(line)
