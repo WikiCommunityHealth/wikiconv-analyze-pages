e17d995441e166fb41f119eaba4dac1e25005b40
nicolatoscan <nicolatoscan99@outlook.it>
2021-05-12 21:21:45 +0200

    fix charts

--- scripts/chart-mean-var.py
+++ scripts/chart-mean-var.py
@@ -1,5 +1,5 @@
 # %% load
-from typing import Any, Callable, Dict, Generator, Iterable, Mapping, List, Tuple, Union, cast
+from typing import Dict, List, Tuple, Union, cast
 import json
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
@@ -32,6 +32,7 @@ em: List[str] = [
     "surprise",
     "trust",
 ]
+showAll: List[Union[str, None]] = cast(List[Union[str, None]], [None]) + cast(List[Union[str, None]], em)
 
 with open('../out.json') as f:
     data = json.load(f)['months']
@@ -82,66 +83,86 @@ print("Fn loaded")
 
 
 
-# %% base
+# %% calculate stuff
 months = [dt.datetime.strptime(month, '%Y-%m').date() for month in data]
 disturbo = np.array([((x - (len(months) / 2)) / (len(months) / 2))**2 for x in range(0, len(months))])
 ys: Dict[str, np.ndarray] = {}
 varss: Dict[str, np.ndarray] = {}
 vol: Dict[str, np.ndarray] = {}
+means: Dict[str, np.number] = {}
+standardScore: Dict[str, np.ndarray] = {}
+meanOverVar: Dict[str, np.ndarray] = {}
 for e in em:
     ys[e] = np.array([data[month][e]['mean'] for month in data])
     varss[e] = (np.array([data[month][e]['var'] for month in data]) / 2)
     vol[e] = np.array([data[month][e]['len'] for month in data])
-vol['global'] = np.array([ sum([data[month][e]['len'] for e in data[month]]) for month in data])
+    means[e] = np.mean(ys[e])
+    standardScore[e] = (ys[e] - means[e]) / np.var(ys[e])
+    meanOverVar[e] = np.divide(ys[e], varss[e], out=np.zeros_like(ys[e]), where=varss[e]!=0)
 
+vol['global'] = np.array([ sum([data[month][e]['len'] for e in data[month]]) for month in data])
+means['global'] = np.mean([means[m] for m in means])
+
+def trimArr(a):
+    return a[102:-8]
+def trimArrDict(d):
+    for e in d:
+        d[e] = d[e][102:-8]
 # Trim
 if True:
-    months = months[102:-8]
-    for e in ys:
-        ys[e] = ys[e][102:-8]
-    for e in varss:
-        varss[e] = varss[e][102:-8]
-    for e in vol:
-        vol[e] = vol[e][102:-8]
-# plotta(months, ys, vol=vol, showOnly='anger')
+    months = trimArr(months)
+    trimArrDict(ys)
+    trimArrDict(varss)
+    trimArrDict(vol)
+    trimArrDict(standardScore)
+    trimArrDict(meanOverVar)
+    # trimArrDict(distanzaDallaMedia)
+    # trimArrDict(toPlotMeans)
+    # mediaDistanzaDallaMedia = trimArr(mediaDistanzaDallaMedia)
 print("Done")
 
+# %% basic charts
+print("Raw data")
+plotta(months, ys, varss, vol=vol, showOnly=showAll)
+# plotta(months, meanOverVar, vol=vol, showOnly=showAll)
+# plotta(months, meanOverVar, vol=vol, showOnly=showAll)
+print("Std score")
+# plotta(months, standardScore, vol=vol)
 
-# %% mean
-means: Dict[str, np.number] = {}
-for e in em:
-    means[e] = np.mean(ys[e])
-globalMean = np.mean([means[m] for m in means])
-means['global'] = globalMean
-# plt.figure(figsize=(100,10))
-# plt.plot(months, mean)
-# plt.show()
-# plt.close()
+
+# %% Means lines to plot
 toPlotMeans: Dict[str, np.ndarray] = {}
 for e in means:
     toPlotMeans[e] = np.array([means[e]] * len(months))
-# print("Medie")
-# plotta(months, toPlotMeans)
+print("Medie")
+plotta(months, toPlotMeans)
 
+# %% Distance from avg line
 distanzaDallaMedia: Dict[str, np.ndarray] = {}
 for e in em:
     distanzaDallaMedia[e] = ys[e] - means[e]
-
+# Avg distance from avg line
 mediaDistanzaDallaMedia = []
 for i in range(0, len(months)):
     mediaDistanzaDallaMedia.append(np.mean([distanzaDallaMedia[e][i] for e in distanzaDallaMedia]))
 distanzaDallaMedia['global'] = np.array(mediaDistanzaDallaMedia)
-# print("Distanza dalla media")
-# plotta(months, distanzaDallaMedia)
-# plotta(months, ys)
 
-print("Res")
-ilRisultato = {}
+print("Distanza dalla media")
+plotta(months, distanzaDallaMedia)
+
+
+# %% res
+ilRisultato: Dict[str, np.ndarray] = {}
 for e in em:
     ilRisultato[e] = distanzaDallaMedia[e] - mediaDistanzaDallaMedia
-ilRisultato['global'] = mediaDistanzaDallaMedia
+ilRisultato['global'] = np.array(mediaDistanzaDallaMedia)
+print("Res")
+plotta(months, ilRisultato, varss, vol=vol, showOnly=showAll)
 
-plotta(months, ilRisultato, varss, vol=vol, showOnly=[None] + em)
-print("Done")
+# %% red std score
+resStdScore: Dict[str, np.ndarray] = {}
+for e in em:
+    resStdScore[e] = (ilRisultato[e] - np.mean(ilRisultato[e])) / np.var(ilRisultato[e])
+plotta(months, resStdScore, vol=vol)
 
 # %%
